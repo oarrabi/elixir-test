ddf91ac997e7e4dac3df893f109fb86d636c1099
Pavel Tsiukhtsiayeu <paveltyk@gmail.com>
2019-02-05 10:00:48 +0300

    Quoted keyword warning fix

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule NewTest.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+     preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
      deps: deps()]
   end
 
